2a2b83da91af8582a641f933fb2cdd7435ced3c7
SmavaProject <katyakravchenka@yahoo.com>
2026-01-27 20:37:20 -0800

    added options

--- src/main/java/com/aiapplication/openai/config/ChatClientConfig.java
+++ src/main/java/com/aiapplication/openai/config/ChatClientConfig.java
@@ -3,6 +3,7 @@ package com.aiapplication.openai.config;
 import com.aiapplication.openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,15 @@ public class ChatClientConfig {
     @Bean
     public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel){
         ChatClient.Builder chatClientBuilder = ChatClient.builder(openAiChatModel);
+        OpenAiChatOptions chatOptions = OpenAiChatOptions.builder()
+                .temperature(0.6)
+                .maxTokens(200)
+                .presencePenalty(0.6)
+                .topP(0.8)
+                .build();
+
         return chatClientBuilder
+                .defaultOptions(chatOptions)
                 .defaultSystem("You are an HR chat bot in a big company. Only respond to HR related questions")
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
                 .defaultUser("How can you help me?")
